ab9bc2ab306cef566a75b111addddbb11cd64436
Taha Ahmed Abdelazim Hussein <71207531+TahaThinks@users.noreply.github.com>
2022-11-06 15:01:25 +0000

    Added the Age algorithm of the Ticket System

--- main.py
+++ main.py
@@ -3,5 +3,12 @@ height = int(input("What is your height in cm? "))
 
 if height >= 120:
     print("You can ride the rollercoaster!")
+    person_age = int(input("How old are you? "))
+    if person_age < 12:
+        print("$5")
+    elif person_age > 12 and person_age <= 18:
+        print("$7")
+    else:
+        print("$12")
 else:
     print("Sorry, You have to grow taller before you can ride.")
